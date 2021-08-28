1e6a1c967868bdfec473a963bf01fd7ed2cce0bc
Akash Ravi <akashkravi@gmail.com>
2021-08-28 15:54:47 -0400

    draws basic grid

--- src/app/part1/part1.component.ts
+++ src/app/part1/part1.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-part1',
   templateUrl: './part1.component.html',
   styleUrls: ['./part1.component.scss']
 })
-export class Part1Component implements OnInit {
+export class Part1Component implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('canvas') canvas: ElementRef | any;
+  private ctx: CanvasRenderingContext2D | any;
+  squares: any = [];
+  
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const canvasEl: HTMLCanvasElement = this.canvas?.nativeElement;
+    this.ctx = canvasEl.getContext('2d');
+    this.drawGrid();
+  }
+
+  // Creates the default grid with gray squares
+  drawGrid() {
+    for (let i = 0; i <= 400; i += 20) {
+      for (let j = 0; j <= 400; j += 20) {
+        this.squares.push({
+          'x': i,
+          'y': j
+        });
+        this.ctx.fillStyle = "gray";
+        this.ctx.fillRect(i, j, 5, 5);
+      }
+    }
   }
 
 }
