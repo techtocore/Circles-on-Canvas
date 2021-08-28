a0bee04ef29394b815daf9d3c045757e20e09ef8
Akash Ravi <akashkravi@gmail.com>
2021-08-28 13:11:50 -0400

    Adds blank components & services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Part1Component } from 'src/app/part1/part1.component';
+import { Part2Component } from 'src/app/part2/part2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'part1', component: Part1Component },
+  { path: 'part2', component: Part2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import "bootstrap";
+import { Part1Component } from './part1/part1.component';
+import { Part2Component } from './part2/part2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Part1Component,
+    Part2Component
   ],
   imports: [
     BrowserModule,
