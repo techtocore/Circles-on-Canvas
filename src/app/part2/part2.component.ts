b80b284effc4d1b2e457b5a14eaf40508d3ea21b
Akash Ravi <akashkravi@gmail.com>
2021-08-28 21:29:23 -0400

    conditionally handles 3 points

--- src/app/part2/part2.component.ts
+++ src/app/part2/part2.component.ts
@@ -68,6 +68,10 @@ export class Part2Component implements AfterViewInit {
     this.getCenter();
     this.getRadius();
 
+    if (this.actualBlueSquares.length === 3) {
+      this.processThreePoints();
+    }
+
     this.ctx.beginPath();
     this.ctx.strokeStyle = "blue";
     this.ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
@@ -123,4 +127,61 @@ export class Part2Component implements AfterViewInit {
 
   }
 
+  processThreePoints() {
+    let x1 = this.actualBlueSquares[0].x;
+    let y1 = this.actualBlueSquares[0].y;
+    let x2 = this.actualBlueSquares[1].x;
+    let y2 = this.actualBlueSquares[1].y;
+    let x3 = this.actualBlueSquares[2].x;
+    let y3 = this.actualBlueSquares[2].y;
+
+    let x12 = (x1 - x2);
+    let x13 = (x1 - x3);
+    let y12 = (y1 - y2);
+    let y13 = (y1 - y3);
+    let y31 = (y3 - y1);
+    let y21 = (y2 - y1);
+    let x31 = (x3 - x1);
+    let x21 = (x2 - x1);
+
+    //x1^2 - x3^2
+    let sx13 = Math.pow(x1, 2) - Math.pow(x3, 2);
+
+    // y1^2 - y3^2
+    let sy13 = Math.pow(y1, 2) - Math.pow(y3, 2);
+
+    let sx21 = Math.pow(x2, 2) - Math.pow(x1, 2);
+    let sy21 = Math.pow(y2, 2) - Math.pow(y1, 2);
+
+    let f = ((sx13) * (x12)
+      + (sy13) * (x12)
+      + (sx21) * (x13)
+      + (sy21) * (x13))
+      / (2 * ((y31) * (x12) - (y21) * (x13)));
+    let g = ((sx13) * (y12)
+      + (sy13) * (y12)
+      + (sx21) * (y13)
+      + (sy21) * (y13))
+      / (2 * ((x31) * (y12) - (x21) * (y13)));
+
+    let c = -(Math.pow(x1, 2)) -
+      Math.pow(y1, 2) - 2 * g * x1 - 2 * f * y1;
+
+    // eqn of circle is
+    // x^2 + y^2 + 2*g*x + 2*f*y + c = 0
+    // where centre is (x = -g, y = -f) and radius r
+    // as r^2 = h^2 + k^2 - c
+    let x = -g;
+    let y = -f;
+    let sqr_of_r = x * x + y * y - c;
+
+    // r is the radius
+    if (!isNaN(sqr_of_r)) {
+      this.radius = Math.sqrt(sqr_of_r);
+      this.center = {
+        'x': x,
+        'y': y
+      };
+    }
+  }
 }
